c7f37bcac6354b888c988663f2e62b5d0eb13833
Max <m4ybe958@gmail.com>
2023-10-23 15:49:06 +0200

    node registering, idenitiy verification, random key generation

--- node.py
+++ node.py
@@ -1,4 +1,4 @@
-import socket, threading, crypto, json
+import socket, threading, crypto, json, base64
 
 class Node:
     def __init__(self, name, port, password=None):
@@ -19,38 +19,40 @@ class Node:
     def accept_connections(self):
         while True:
             peer_socket, peer_address = self.listen_socket.accept()
-            print(f"Peer connected from {peer_address}")
+            print(f"{self.name}: Peer connected to me from {peer_address}")
             self.peers.append(peer_socket)
             threading.Thread(target=self.handle_request, args=(peer_socket,)).start()
 
 
     def handle_request(self, peer_socket):
-        while True:
-            packet = peer_socket.recv(1024)
-            if not packet:
-                break
-            if packet.header == "send":
-                self.handle_send(packet, peer_socket)
-            elif packet.header == "register":
-                self.handle_register(packet, peer_socket)
-            else:
-                peer_socket.send(json.dump({"header": "response", "message": "Error: unknown header"}))
-                peer_socket.close()
-                break
+        packet = json.loads(peer_socket.recv(4096).decode("utf-8"))
+        if not packet:
+            return
+        for key, value in packet.items():
+            if key in ["pubkey", "signature"]:
+                packet[key] = base64.b64decode(value.encode("utf-8"))
+        if packet["header"] == "send":
+            self.handle_send(packet, peer_socket)
+        elif packet["header"] == "register":
+            self.handle_register(packet, peer_socket)
+        else:
+            peer_socket.send(json.dumps({"header": "response", "message": "Error: unknown header"}))
+            peer_socket.close()
 
 
     def handle_register(self, packet, peer_socket):
-        if crypto.verify_signature_with_public_key(packet.pubkey, packet.name, packet.signature):
-            self.peers.append({"name": packet.name, "pubkey": packet.pubkey, "port": packet.port})
-            print(f"Just registered a new guy: {self.peers[-1]}.")
+        if crypto.verify_signature_with_public_key(packet["pubkey"], packet["name"], packet["signature"]):
+            self.peers.append({"name": packet["name"], "pubkey": packet["pubkey"], "port": packet["port"]})
+            print(f"{self.name}: I just registered a new guy:\n{self.peers[-1]}.\n")
+            peer_socket.send(json.dumps({"header": "response", "message": "Success: you got registered bro, happy minin'!"}).encode("utf-8"))
             # and now let everyone know about the new guy
         else:
-            peer_socket.send(json.dump({"header": "response", "message": "Error: keys don't match, get lost hacker!"}))
+            peer_socket.send(json.dumps({"header": "response", "message": "Error: keys don't match, get lost hacker!"}).encode("utf-8"))
         peer_socket.close()
     
 
     def handle_send(self, packet, peer_socket):
-        print(f"{self.name}: Received data from {packet.name}\n{packet.data}\n")
+        print(f"{self.name}: Received data from {packet['name']}\n{packet['data']}\n")
         peer_socket.close()
 
 
@@ -66,7 +68,7 @@ class Node:
             connecting_socket.connect(("localhost", port))
             connecting_socket.send(json.dumps(packet))
         except:
-            print("Lost connection to peer.2")
+            print("Lost connection to peer when sending data.")
         connecting_socket.close()
 
 
@@ -76,17 +78,15 @@ class Node:
         packet = {
             "header": "register",
             "name": self.name,
-            "pubkey": self.pubkey,
+            "pubkey": base64.b64encode(self.pubkey).decode("utf-8"),
             "port": self.port,
-            "signature": crypto.sign_with_private_key(self.privkey, self.name)
+            "signature": base64.b64encode(crypto.sign_with_private_key(self.privkey, self.name)).decode("utf-8")
         }
         try:
             registering_socket.connect(("localhost", port))
-            print("connected successfully")
-            registering_socket.send(packet)
-            print("sent successfully")
-            response = registering_socket.recv(1024)
-            print(json.loads(response))
+            registering_socket.send(json.dumps(packet).encode("utf-8"))
+            response = json.loads(registering_socket.recv(4096))
+            print(f"{self.name}: I got this response after registering:\n{response}\n")
         except Exception as e:
-            print(f"Lost connection to peer. {e}")
+            print(f"Lost connection to peer when registering. {e}")
         registering_socket.close()
